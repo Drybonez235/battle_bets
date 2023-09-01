8fdcd69ab5a42be47cd6c5d795243d99049791ee
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-09-01 12:58:27 -0500

    There are some bugs in here

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const db = database.createDbConnection()
 const app = express()
 const port = 8088
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//app.get('/', (req, res, next) => {
+//    res.send('Hello World!');
+//    next();
+//})
+
+const add_row_express = () => {
+    database.add_row(db, 'Drybonez', 500, "09/01/2023", "Streamer_ID", "Sessions_number2");
+};
+const read_row_express =  ()=> {
+    database.read_record(db);
+    
+};
 
 app.listen(port, () => {
-    console.log('Listening on port ${port}')
-    database.add_row(db)
-    database.read_record(db)
+    console.log('Listening on port $port', {port})
+    //database.read_record(db)
 })
+//app.use(const db = database.createDbConnection())
+app.use(add_row_express)
+app.use(read_row_express)
+
+await add_row_express();
+read_row_express()
--- sqlite3_helper.js
+++ sqlite3_helper.js
@@ -26,22 +26,31 @@ function createTable(db) {
         ID INTEGER,
         user_name VARCHAR(50),
         total_points INTEGER,
-        play_date DATE,
+        play_date VARCHAR(12),
         streamer_id VARCHAR(12),
         session_id VARCHAR(20)
-        )
+        );
         `);
 }
 
-function add_row(db) {
+function add_row(db, user_name, points, date, streamer_id, session_id) {
     db.run(`
-    INSERT INTO leaderboard VALUES(1 ,'Jonathan lewis', 500, 2023/08/23, '#tytytyt', 'Sessions')    
-    `)
+    INSERT INTO leaderboard VALUES(1, $user_name_, $points_, $date_ , $streamer_id_, $session_id_);    
+    `, {
+        user_name_ : user_name,
+        points_ : points,
+        date_ : date,
+        streamer_id_ : streamer_id,
+        session_id_ : session_id
+    })
 }
 
 function read_record(db) {
     text = db.all('SELECT * FROM leaderboard;', function(err, table) {
         console.log(table);
+        let ID = table[0].ID;
+        //console.log(ID)
+
 })
 }
 
