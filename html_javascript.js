fb4544ff56aabf9a4faf6e9a3fe384a8006c4198
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-09-06 20:25:54 -0500

    Why is this taking so long...

--- html_javascript.js
+++ html_javascript.js
@@ -19,9 +19,9 @@ const add_crown = (tag_id, win_lose) => {
 
     let current_crown = document.getElementById(tag_id);
     if(win_lose == "win"){
-        current_crown.outerHTML = "<img id='" + tag_id + "' class='inline object-contain h-8' onclick=\"remove_crown('"+ tag_id + "', 'win')\" src='" + red_crown + "'>";
+        current_crown.outerHTML = "<img id='" + tag_id + "' class='cursor-pointer inline object-contain h-8' onclick=\"remove_crown('"+ tag_id + "', 'win')\" src='" + red_crown + "'>";
     } else{
-        current_crown.outerHTML = "<img id='" + tag_id + "' class='inline object-contain h-8' onclick=\"remove_crown('"+ tag_id + "', 'lose')\" src='" + blue_crown + "'>";
+        current_crown.outerHTML = "<img id='" + tag_id + "' class='cursor-pointer inline object-contain h-8' onclick=\"remove_crown('"+ tag_id + "', 'lose')\" src='" + blue_crown + "'>";
     }
 }
 
@@ -31,18 +31,16 @@ const remove_crown = (tag_id, win_lose) => {
 
     let current_crown = document.getElementById(tag_id);
     if(win_lose == "win"){
-        current_crown.outerHTML = "<img id='" + tag_id+ "' class='inline object-contain h-8' onclick=\"add_crown('" + tag_id + "', 'win')\" src='" + red_crown + "'>";
+        current_crown.outerHTML = "<img id='" + tag_id+ "' class='cursor-pointer inline object-contain h-8' onclick=\"add_crown('" + tag_id + "', 'win')\" src='" + red_crown + "'>";
     } else{
-        current_crown.outerHTML = "<img id='" + tag_id+ "' class='inline object-contain h-8' onclick=\"add_crown('" + tag_id + "', 'lose')\" src='" + blue_crown + "'>";
+        current_crown.outerHTML = "<img id='" + tag_id+ "' class='cursor-pointer inline object-contain h-8' onclick=\"add_crown('" + tag_id + "', 'lose')\" src='" + blue_crown + "'>";
     }
 }
 
 const reset_bet = () => {
     let win_lose_toggle = document.getElementById("win_loss_toggle_input_value");
     let crowns_taken_toggle = document.getElementById("crowns_taken_toggle_1");
-    let crowns_taken_value = 0;
     let crowns_lost_toggle = document.getElementById("crowns_lost_toggle_1");
-    let crowns_lost_value = 0;
     let bet_amount = document.getElementById("points_bet_1");
 
      win_lose_toggle.checked = true;
--- user_interface_logic.js
+++ user_interface_logic.js
@@ -8,7 +8,7 @@ let points_bet = 1000;
 
 game_tracker_array[3] = ["Jonathan", 1, 2];
 
-game("Jonathan", 1, 2, "10")
+//game("Jonathan", 1, 2, "10")
 
 function game(player_id_string, win_loss_bool, crowns_take_int, game_time){
     console.log("Game Function");
@@ -56,4 +56,30 @@ function winning_points_calculator(win_rate, points_bet){
     console.log(win_rate_multiplier + " " + parlay_multiplier )
     console.log((win_rate_multiplier * points_bet) * parlay_multiplier);
     return win_rate_multiplier * points_bet * parlay_multiplier;
+}
+
+function calculator_gun(){
+    let number = parseInt(document.getElementById("points_bet_1").value);
+    let win_rate = parseFloat(document.getElementById("streamer_win_rate_number").innerHTML);
+    let potential_points = document.getElementById("calculated_bet_number");
+    let crowns_taken_checked = document.getElementById("crowns_won_toggle_1").checked ? 1:0;
+    let crowns_lost_checked = document.getElementById("crowns_lost_toggle_1").checked  ? 1:0;
+    let win_lose_toggle = document.getElementById("win_loss_toggle_input_value").checked ? 1:0;
+    let win_rate_multiplier = 0.0;
+
+    if(win_lose_toggle == 1){//They say ox will wins
+        //If oxes win rate is at 0% they should get more points
+        win_rate_multiplier = 2 - win_rate ;
+    } else {//This is if od win rate is 100 and they predict he loses. 
+        win_rate_multiplier = 1 + win_rate;
+    }
+
+    console.log(win_lose_toggle);
+    console.log(crowns_taken_checked);
+    console.log(crowns_lost_checked);
+    console.log(number);
+    console.log(win_rate);
+
+    potential_points.innerHTML = number * win_rate_multiplier * (crowns_taken_checked + crowns_lost_checked + 1);
+    return;
 }
