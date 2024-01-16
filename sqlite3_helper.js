64844cbfda68665be27b55c1dcf494d1a0ed03df
Jonathan Lewis <jonathan.lewis@welch.edu>
2024-01-16 16:54:52 -0600

    I DID IT! I FIGURED OUT ASYNC AND PROMISES

--- sqlite3_helper.js
+++ sqlite3_helper.js
@@ -16,10 +16,7 @@ function createDbConnection() {
            console.log("We created the table...") 
             createTable(db);
   });
-  console.log("Connection with SQLite has been established");
-  return db;
-}
-}
+}}
 
 function createTable() {
     db.run(`CREATE TABLE leaderboard (
@@ -45,53 +42,105 @@ function add_row(user_name, points, streamer_id, session_id) {
     db.close();
 }
 
-function update_row(user_name, session_id, streamer_id, points){
-    const db = createDbConnection();
-    db.run(`
-   UPDATE leaderboard SET total_points = $points_ WHERE user_name = $user_name_ AND streamer_id = $streamer_id_ AND session_id = $session_id_;
-    `, {
+function update_row(user_name, streamer_id, session_id, total_points){
+    return new Promise((resolve, reject) => {
+        const db = createDbConnection();
+        db.run(`UPDATE leaderboard SET total_points = $points_ WHERE (user_name = $user_name_ AND session_id = $session_id_ AND streamer_id = $streamer_id_);`
+        , {
+            $points_ : total_points, 
+            $user_name_ : user_name,
+            $session_id_ : session_id,
+            $streamer_id_ : streamer_id
+        }, function (err) {
+            if (err != null) {
+                reject()
+            } else {
+                resolve(err);
+            }
+        });
+    });
+}
+
+function read_record(user_name, streamer_id, session_id) {
+    return new Promise((resolve, reject) => {
+        const db = createDbConnection();
+       db.all('SELECT user_name, total_points FROM leaderboard WHERE user_name = $user_name_ AND streamer_id = $streamer_id_ AND session_id = $session_id_;',
+       {
         $user_name_ : user_name,
         $session_id_ : session_id,
-        $streamer_id_ : streamer_id,
-        $points_ : points
+        $streamer_id_ : streamer_id
+       }, 
+       (err, rows) => {
+        if(err){
+            reject(err);
+        } else {
+            resolve(rows);
+        }
+       }) 
     })
-    db.close();
 }
 
-function read_record() {
-    const db = createDbConnection();
-    text = db.all('SELECT * FROM leaderboard;', function(err, table) {
-        console.log(table);
-        //let ID = table[0].ID;
-        //console.log(ID)
-
-})
-db.close();
-}
-
-function read_top_ten(session_id, streamer_id){
-    const db = createDbConnection();
-    return_value = [];
-     value = db.all(`SELECT user_name, total_points FROM leaderboard WHERE session_id = $session_id_ AND streamer_id = $streamer_id_ ORDER BY total_points DESC LIMIT 10;
-    `,{
+function promise_read_top_ten(session_id, streamer_id){
+    return new Promise((resolve, reject) => {
+        const db = createDbConnection();
+        const return_data = [];
+        //This is sql statment
+        db.each(`SELECT user_name, total_points FROM leaderboard WHERE session_id = $session_id_ AND streamer_id = $streamer_id_ ORDER BY total_points DESC LIMIT 10;`,
+        //This is the parameters
+        {
         $session_id_ : session_id,
         $streamer_id_ : streamer_id
-    }, function(err, table){
-        for(i=0;i < 10; i++){
-            return_value.push(table[i]);
-            //console.log(table[i]);
-        }
-        //console.log(return_value);
-    })
-    db.close();
-    return value;
+        },
+        //This is the callback
+         (err, rows)=>{
+            if(err){
+                reject(err);
+            } else{
+            return_data.push(rows);
+            }
+        },
+        //this is on completion.
+         (err, n) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(return_data)
+            }
+         //end of db.each stamtent   
+        });
+        //end of promise
+    });
+    //end of function.
 }
 
+function promise_read_top_ten_all(session_id, streamer_id){
+    return new Promise((resolve, reject) => {
+        const db = createDbConnection();
+        //This is sql statment
+        db.all(`SELECT user_name, total_points FROM leaderboard WHERE session_id = $session_id_ AND streamer_id = $streamer_id_ ORDER BY total_points DESC LIMIT 10;`,
+        //This is the parameters
+        {
+        $session_id_ : session_id,
+        $streamer_id_ : streamer_id
+        },
+        //This is the callback
+         (err, rows)=>{
+            if(err){
+                reject(err);
+            } else{
+            resolve(rows);
+            }  
+        });
+        //end of promise
+    });
+    //end of function.
+}
 
 module.exports = {
 createDbConnection,
 read_record,
 add_row,
 update_row,
-read_top_ten
+promise_read_top_ten,
+promise_read_top_ten_all
 }
